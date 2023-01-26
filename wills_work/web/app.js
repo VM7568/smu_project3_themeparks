75c77d432c76c8af4930262a27763d498e16bc4a
omar <omar.zaher91@gmail.com>
2023-01-25 18:06:49 -0600

    test

--- wills_work/web/app.js
+++ wills_work/web/app.js
@@ -1,12 +1,40 @@
-const dataPromise = d3.csv('C:/Users/Will/Desktop/smubootcamp/github/proj3/smu_project3_themeparks/Will's folder\web\sample_df.csv');
+data = []
+
+d3.csv("sample_df.csv").then(function (data) {
+    console.log(data);
+    doWork(data)
+});
 
 
+function doWork(data) {
+    let trace = {
+        type: 'bar',
+        x: data.map(x => x.device_category),
+        y: data.map(x => +x.num_injured),
 
+        box: {
+            visible: true
+        },
+        line: {
+            color: 'blue',
+        },
+        meanline: {
+            visible: true
+        }
+    }
 
+    let traces = [trace]
+    var layout = {
+        title: "Device Accidents",
+        yaxis: {
+            zeroline: false
+        }
+    }
 
+    Plotly.newPlot('bubble', traces, layout);
 
-
-
+}
+//
 
 
 
